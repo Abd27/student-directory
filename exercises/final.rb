75b0550b0f4e0c9ac311fb5e3f7a9e846761aaed
Abdur Rehman <wabdulrehman@yahoo.com>
2021-03-25 14:27:30 +0000

    add a final.rb which will successively update all the excersies in one file

---
+++ exercises/final.rb
@@ -0,0 +1,51 @@
+def input_students
+  puts "Please enter the names of students"
+  puts "To finish, just git return twice"
+  #create an empty array.
+  students = []
+  #get the first name.
+  name = gets.chomp
+  #while name is not empty, repeat this code.
+  while !name.empty? do
+    #add a student hash to our array.
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from user.
+    name = gets.chomp
+  end
+  #return the student array.
+  students
+end
+def specific(names)
+  specific_students = []
+  puts "Enter the initial letter"
+  input = gets.chomp.upcase
+  names.each do |student|
+    if student[:name][0] == input
+      specific_students.push(student)
+    end
+  end
+  specific_students
+end  
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print_specific(names)
+  names.each.with_index(1) do |student, index|    
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)}" 
+  end 
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students for this catagory"
+end
+
+       
+
+#now calling the methods
+students = specific(input_students)
+
+print_header
+print_specific(students)
+print_footer(students)
+
